edecc93363e8f42cbf50b02efb1c853a4622cf3f
Nguyen Duy Khanh <nguyenkhanh17112003@gmail.com>
2026-02-23 08:19:14 +0700

    Commit local changes trước khi pull từ master

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,10 @@
-import type { NextConfig } from "next";
+import { createMDX } from 'fumadocs-mdx/next';
 
-const nextConfig: NextConfig = {};
+const config = {
+  reactStrictMode: true,
+};
 
-export default nextConfig;
+const withMDX = createMDX({
+});
+
+export default withMDX(config);
