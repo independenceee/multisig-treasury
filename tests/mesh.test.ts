b583314f2657cf0deab6d4ae56b5fe1b48a5bf78
Nguyen Duy Khanh <nguyenkhanh17112003@gmail.com>
2026-04-21 19:31:17 +0700

    chore: update dependencies and adjust multisig treasury allowance
    - Added "vercel" dependency to package.json
    - Changed allowance from 11 to 10 in mesh.test.ts for multisig treasury
    - Removed unnecessary owner addresses in mesh.test.ts

--- tests/mesh.test.ts
+++ tests/mesh.test.ts
@@ -34,7 +34,7 @@ describe("A multisig treasury is a shared fund where spending requires approval 
         const meshTxBuilder: MeshTxBuilder = new MeshTxBuilder({
             meshWallet: meshWallet,
             threshold: 2,
-            allowance: 11 * DECIMAL_PLACE,
+            allowance: 10 * DECIMAL_PLACE,
             name: "Aiken Course 2030",
         });
 
@@ -43,9 +43,6 @@ describe("A multisig treasury is a shared fund where spending requires approval 
             owners: [
                 "addr_test1qz45qtdupp8g30lzzr684m8mc278s284cjvawna5ypwkvq7s8xszw9mgmwpxdyakl7dgpfmzywctzlsaghnqrl494wnqhgsy3g",
                 "addr_test1qr39uar0u87xrmptw0f8ryx5mp3scvc3pkehp57yj5zhugxdgese6p77sy9hk0rqc5wqd6n8vmfyqq9f7sdfz9dm0azqzmmdew",
-                "addr_test1qqy0z4ekhv8gcnmvkeakkaher82rlrx2yu9y79cjf4r704pqg73fhf002takqewlvjcy39dellyumg43f08uea0p6mps7pw77f",
-                "addr_test1qrpfhvwrmq0y27k2elu0seh65w6kwyxxee6sq7f9d2ax62e8wm6fj2y63rp3kql4skhu2wyt0uml07w2pggzpzh95ugqk9j5d9",
-                "addr_test1qpm9a92nk6grxwsxluqyjt9xd3cjcps90fjv8txm4spd6tv4mkujqpc7fzlvqu40kyvzh6fxmqp0578uk564ffqtfr7s9ppr9y",
             ],
         });
 
