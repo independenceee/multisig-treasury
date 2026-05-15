51bd049be0d6e375b4d0c16583ba9e6afbe72d10
NguIndependence <nguyenkhanh17112003@gmail.com>
2026-05-15 14:09:05 +0700

    fix: update environment variable for app network and adjust related references
    chore: downgrade Aiken compiler version in Plutus contract
    
    fix: update compiled code and hash in Plutus contract
    
    refactor: modify identity factory validator to simplify Init redeemer handling
    
    refactor: remove issuer parameter from multisig treasury validator
    
    chore: update package.json for contract check and build scripts, and upgrade vercel dependency
    
    test: adjust mesh tests for multisig treasury with updated allowance and network references

--- tests/mesh.test.ts
+++ tests/mesh.test.ts
@@ -1,22 +1,22 @@
 import { MeshWallet } from "@meshsdk/core";
 import { MeshTxBuilder } from "../txbuilders/mesh.txbuilder";
 import { blockfrostProvider } from "../providers/cardano";
-import { APP_MNEMONIC, APP_NETWORK_ID } from "../constants/enviroments";
+import { APP_MNEMONIC, APP_NETWORK, APP_NETWORK_ID } from "../constants/enviroments";
 import { DECIMAL_PLACE } from "../constants/common.constant";
 import { getHistories } from "@/services/treasury";
 
 describe("A multisig treasury is a shared fund where spending requires approval from at least m of n participants, with a predefined spending limit for security.", function () {
     let meshWallet: MeshWallet;
 
-    // account 1 - addr_test1qz45qtdupp8g30lzzr684m8mc278s284cjvawna5ypwkvq7s8xszw9mgmwpxdyakl7dgpfmzywctzlsaghnqrl494wnqhgsy3g
-    // account 2 - addr_test1qr39uar0u87xrmptw0f8ryx5mp3scvc3pkehp57yj5zhugxdgese6p77sy9hk0rqc5wqd6n8vmfyqq9f7sdfz9dm0azqzmmdew
-    // account 3 - addr_test1qqy0z4ekhv8gcnmvkeakkaher82rlrx2yu9y79cjf4r704pqg73fhf002takqewlvjcy39dellyumg43f08uea0p6mps7pw77f
-    // account 4 - addr_test1qrpfhvwrmq0y27k2elu0seh65w6kwyxxee6sq7f9d2ax62e8wm6fj2y63rp3kql4skhu2wyt0uml07w2pggzpzh95ugqk9j5d9
-    // account 5 - addr_test1qpm9a92nk6grxwsxluqyjt9xd3cjcps90fjv8txm4spd6tv4mkujqpc7fzlvqu40kyvzh6fxmqp0578uk564ffqtfr7s9ppr9y
+    // account 0 - addr_test1qz45qtdupp8g30lzzr684m8mc278s284cjvawna5ypwkvq7s8xszw9mgmwpxdyakl7dgpfmzywctzlsaghnqrl494wnqhgsy3g
+    // account 1 - addr_test1qr39uar0u87xrmptw0f8ryx5mp3scvc3pkehp57yj5zhugxdgese6p77sy9hk0rqc5wqd6n8vmfyqq9f7sdfz9dm0azqzmmdew
+    // account 2 - addr_test1qqy0z4ekhv8gcnmvkeakkaher82rlrx2yu9y79cjf4r704pqg73fhf002takqewlvjcy39dellyumg43f08uea0p6mps7pw77f
+    // account 3 - addr_test1qrpfhvwrmq0y27k2elu0seh65w6kwyxxee6sq7f9d2ax62e8wm6fj2y63rp3kql4skhu2wyt0uml07w2pggzpzh95ugqk9j5d9
+    // account 4 - addr_test1qpm9a92nk6grxwsxluqyjt9xd3cjcps90fjv8txm4spd6tv4mkujqpc7fzlvqu40kyvzh6fxmqp0578uk564ffqtfr7s9ppr9y
 
     beforeEach(async function () {
         meshWallet = new MeshWallet({
-            accountIndex: 3,
+            accountIndex: 0,
             networkId: APP_NETWORK_ID,
             fetcher: blockfrostProvider,
             submitter: blockfrostProvider,
@@ -30,12 +30,12 @@ describe("A multisig treasury is a shared fund where spending requires approval 
     jest.setTimeout(600000000);
 
     test("Init", async function () {
-        // return;
+        return;
         const meshTxBuilder: MeshTxBuilder = new MeshTxBuilder({
             meshWallet: meshWallet,
             threshold: 2,
-            allowance: 10 * DECIMAL_PLACE,
-            name: "Aiken Course 2030",
+            allowance: 15 * DECIMAL_PLACE,
+            name: "Aiken Course 2026",
         });
 
         const unsignedTx: string = await meshTxBuilder.init({
@@ -50,7 +50,7 @@ describe("A multisig treasury is a shared fund where spending requires approval 
         const txHash = await meshWallet.submitTx(signedTx);
         await new Promise<void>(function (resolve) {
             blockfrostProvider.onTxConfirmed(txHash, () => {
-                console.log("https://preview.cexplorer.io/tx/" + txHash);
+                console.log("https://" + APP_NETWORK + ".cexplorer.io/tx/" + txHash);
                 resolve();
             });
         });
@@ -61,18 +61,18 @@ describe("A multisig treasury is a shared fund where spending requires approval 
         const meshTxBuilder: MeshTxBuilder = new MeshTxBuilder({
             meshWallet: meshWallet,
             threshold: 2,
-            allowance: 10 * DECIMAL_PLACE,
+            allowance: 15 * DECIMAL_PLACE,
             name: "Aiken Course 2026",
         });
         const unsignedTx: string = await meshTxBuilder.deposit({
-            quantity: "10000000",
+            quantity: "15000000",
         });
 
         const signedTx = await meshWallet.signTx(unsignedTx, true);
         const txHash = await meshWallet.submitTx(signedTx);
         await new Promise<void>(function (resolve) {
             blockfrostProvider.onTxConfirmed(txHash, () => {
-                console.log("https://preview.cexplorer.io/tx/" + txHash);
+                console.log("https://" + APP_NETWORK + ".cexplorer.io/tx/" + txHash);
                 resolve();
             });
         });
@@ -83,7 +83,7 @@ describe("A multisig treasury is a shared fund where spending requires approval 
         const meshTxBuilder: MeshTxBuilder = new MeshTxBuilder({
             meshWallet: meshWallet,
             threshold: 2,
-            allowance: 10 * DECIMAL_PLACE,
+            allowance: 15 * DECIMAL_PLACE,
             name: "Aiken Course 2026",
         });
 
@@ -93,7 +93,7 @@ describe("A multisig treasury is a shared fund where spending requires approval 
         const txHash = await meshWallet.submitTx(signedTx);
         await new Promise<void>(function (resolve) {
             blockfrostProvider.onTxConfirmed(txHash, () => {
-                console.log("https://preview.cexplorer.io/tx/" + txHash);
+                console.log("https://" + APP_NETWORK + ".cexplorer.io/tx/" + txHash);
                 resolve();
             });
         });
@@ -104,18 +104,18 @@ describe("A multisig treasury is a shared fund where spending requires approval 
         const meshTxBuilder: MeshTxBuilder = new MeshTxBuilder({
             meshWallet: meshWallet,
             threshold: 2,
-            allowance: 10 * DECIMAL_PLACE,
+            allowance: 15 * DECIMAL_PLACE,
             name: "Aiken Course 2026",
         });
         const unsignedTx: string = await meshTxBuilder.execute({
-            amount: "10000000",
+            amount: "15000000",
         });
 
         const signedTx = await meshWallet.signTx(unsignedTx, true);
         const txHash = await meshWallet.submitTx(signedTx);
         await new Promise<void>(function (resolve) {
             blockfrostProvider.onTxConfirmed(txHash, () => {
-                console.log("https://preview.cexplorer.io/tx/" + txHash);
+                console.log("https://" + APP_NETWORK + ".cexplorer.io/tx/" + txHash);
                 resolve();
             });
         });
@@ -126,7 +126,7 @@ describe("A multisig treasury is a shared fund where spending requires approval 
         const meshTxBuilder: MeshTxBuilder = new MeshTxBuilder({
             meshWallet: meshWallet,
             threshold: 2,
-            allowance: 10 * DECIMAL_PLACE,
+            allowance: 15 * DECIMAL_PLACE,
             name: "Aiken Course 2026",
         });
 
@@ -136,7 +136,7 @@ describe("A multisig treasury is a shared fund where spending requires approval 
         const txHash = await meshWallet.submitTx(signedTx);
         await new Promise<void>(function (resolve) {
             blockfrostProvider.onTxConfirmed(txHash, () => {
-                console.log("https://preview.cexplorer.io/tx/" + txHash);
+                console.log("https://" + APP_NETWORK + ".cexplorer.io/tx/" + txHash);
                 resolve();
             });
         });
